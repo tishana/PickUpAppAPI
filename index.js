a65532ca222ed2ef3d7dd444e5c38a4bb277c979
MJ Sandoval <salssedan@gmail.com>
2018-12-03 17:28:33 -0500

    changes

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const app = express();
 
 app.use(parser.urlencoded({ extended: true }));
 
-app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
+
+app.get('/api/drivers', (req, res) => {
+    Driver.find()
+        .then((drivers) => {
+            res.json(drivers)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 app.listen(8000, () => console.log("on port 8000..."));
