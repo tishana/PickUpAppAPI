033fb6dc877cb2b7157b07db48f31983decf9937
Tishana <tishana.trainor@gmail.com>
2018-12-04 14:50:47 -0500

    added get request for indiv ords and a delete req for cancelling orders

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const parser = require("body-parser")
 const cors = require('cors')
 const Driver = require('./models/Driver')
 const Order = require('./models/Order')
-const orderData = require('./db/orderData.json')
+// const orderData = require('./db/orderData.json')// see below TT
 
 const app = express()
 
@@ -26,7 +26,24 @@ app.get('/api/drivers', (req, res) => {
 })
 
 app.get('/api/orders', (req, res) => {
-    res.json(orderData)
+    // res.json(orderData) // we shouldn't use this because it will only show the data in the seed file, and not ALL Orders TT
+    Order.find()
+        .then((orders) => {
+            res.json(orders)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('api/orders/:id', (req, res) => {
+    Order.findById(req.params.id)
+        .then(order => {
+            res.json(order)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 })
 
 app.post('/api/orders', (req, res) => {
@@ -35,6 +52,16 @@ app.post('/api/orders', (req, res) => {
             res.json(order)
         })
         .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.delete('/api/orders/delete/:id', (req, res) => {
+    Order.findOneAndRemove({ _id: req.params.id })
+        .then(() => {
+            res.redirect('/confirm')//placeholder for cancelled orders
+        })
+        .catch(err => {
             console.log(err)
         })
 })
